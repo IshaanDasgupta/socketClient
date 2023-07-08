54197a1f83f93b6ccc480310cc7c708be3de0131
Ishaan Dasgupta <dasgupta.ishan@gmail.com>
2023-07-09 02:21:28 +0530

    fixing a bug in home

--- src/pages/home/Topbar/Topbar.jsx
+++ src/pages/home/Topbar/Topbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import Logo from "../../../static/videoConferencing/Icon.svg";
+import {useNavigate} from "react-router-dom";
 
 import styles from "./topbar.module.css";
 
@@ -8,16 +9,18 @@ const Topbar = () => {
 
     const userProfilePic = useSelector((state) => state.userDetails.profilePic);
 
+    const navigate = useNavigate();
+
     return (
         <div className={styles.container}>
             <img src={Logo} alt="" className={styles.logo}/>     
-            {userProfilePic.length > 0 ?       
+            {userProfilePic && userProfilePic.length > 0 ?       
                     <div className={styles.profilePic} dangerouslySetInnerHTML={{__html:userProfilePic}} />
                 :
-                <div className={styles.flex}>
-                    <div className={styles.button1}>Sign Up</div>
-                    <div className={styles.button2}>Login</div>
-                </div>
+                    <div className={styles.flex}>
+                        <div className={styles.button1} onClick={() => {navigate("/register")}} >Sign Up</div>
+                        <div className={styles.button2} onClick={() => {navigate("/login")}} >Login</div>
+                    </div>
             }
         </div>
     )
